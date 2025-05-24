4d6f48d04c550861c845383db0e48ac48abf4a4c
AldyRevigustian <aldyrevig@gmail.com>
2025-05-24 23:50:10 +0700

    Light Theme

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -472,7 +472,7 @@
                             refreshButton.disabled = false;
                         });
                 }
-            });
+                });
         </script>
     @endpush
 </x-app-layout>
