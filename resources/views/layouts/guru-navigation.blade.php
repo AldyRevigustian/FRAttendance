b806a9b503d25c2a3c3ba7ffe4c58495c06eec21
AldyRevigustian <aldyrevig@gmail.com>
2025-05-26 21:23:17 +0700

    Dashboard

--- resources/views/layouts/guru-navigation.blade.php
+++ resources/views/layouts/guru-navigation.blade.php
@@ -11,11 +11,11 @@
                 </div>
 
                 <!-- Navigation Links -->
-                {{-- <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('guru.dashboard')" :active="request()->routeIs('guru.dashboard')">
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('guru.dashboard', ['kelas_terpilih' => session('kelas_aktif')])" :active="request()->routeIs('guru.dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                </div> --}}
+                </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('guru.siswa', ['kelas_terpilih' => session('kelas_aktif')])" :active="request()->routeIs('guru.siswa*')">
                         {{ __('Siswa') }}
